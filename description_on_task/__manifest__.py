c9d31c5a9f0a08668741012f2b17fa454a1beba4
parth <parth@caretit.com>
2019-06-20 15:52:53 +0530

    [ADD] module description on task

---
+++ description_on_task/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Part of Caret IT Solutions Pvt. Ltd. (Website: www.caretit.com).
+# See LICENSE file for full copyright and licensing details.
+#
+##############################################################################
+{
+    'name': "Task Description",
+    'summary': """""",
+    'description': """
+
+    """,
+    'author': 'Caret IT Solutions Pvt. Ltd.',
+    'website': 'http://www.caretit.com',
+    'category': 'Project',
+    'version': '12.0',
+    'depends': ['project'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/project_views.xml',
+    ],
+    'demo': [
+    ],
+}
