f8d0f6d843410a191f83106ec07cf32d377967c1
parth <parth@caretit.com>
2019-07-01 14:08:08 +0530

    [IMP] some changes

--- task_advance/models/machine.py
+++ task_advance/models/machine.py
@@ -35,21 +35,24 @@ class Capacity(models.Model):
         for record in self:
             record.remain_capacity = record.capacity - record.total_Production_time
 
-    @api.multi
-    def name_get(self):
-        res = []
-        for capacity in self:
-            name = '%s (%s)' % (capacity.name, '{0:02.0f}:{1:02.0f}'.format(*divmod(
-                capacity.remain_capacity * 60, 60)))
-            res.append((capacity.id, name))
-        return res
+    # @api.multi
+    # def name_get(self):
+    #     res = []
+    #     for capacity in self:
+    #         name = '%s (%s)' % (capacity.name, '{0:02.0f}:{1:02.0f}'.format(*divmod(
+    #             capacity.remain_capacity * 60, 60)))
+    #         res.append((capacity.id, name))
+    #     return res
 
     def _join_name_date(self, name, date):
-        nameContent = []
-        for nm in name.split(' '):
-            nameContent.append(nm[:2])
-            nameContent.append(nm[-1:])
-        return''.join(nameContent) + ' ' + date.strftime('%d-%m/%W')
+        # nameContent = []
+        # for nm in name.split(' '):
+        #     nameContent.append(nm[:2])
+        #     nameContent.append(nm[-1:])
+        # return''.join(nameContent) + ' ' + date.strftime('%d-%m/%W')
+        nameContent = name[:2] + name[-1:]
+        weekDay = ['MA', 'DI', 'WO', 'DO', 'VR', 'ZA', 'ZO']
+        return nameContent.upper() + ' ' + date.strftime('%d-%m/%W') + weekDay[date.weekday()]
 
     @api.model
     def create(self, vals):
